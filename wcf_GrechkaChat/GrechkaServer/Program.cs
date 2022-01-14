6b910d501bb16f928fe19f97ae9d3e8486f326a0
Flamesone2 <sergbasov.com@gmail.com>
2022-01-14 04:54:13 +0300

    переделал весь сервер

---
+++ wcf_GrechkaChat/GrechkaServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrechkaServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var host = new ServiceHost(typeof(wcf_GrechkaChat.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Сервер стартовал!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
